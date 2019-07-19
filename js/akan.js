6ca1fbcbd1c3b4a7415befde936a79611582d696
Imanirakarama Alexianne <imanirax@yahoo.fr>
2019-07-19 11:20:23 +0200

    added console.log for the day of the week and then rounded it off to the nearest whole number using math.round

--- js/akan.js
+++ js/akan.js
@@ -3,4 +3,5 @@ var monthOfBirth=parseFloat(document.getElementById("month").value);
 var yearOfBirth=parseFloat(document.getElementById("year").value);
 var cc=(year-1)/100+1;
 var dayOfTheWeek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
-  
+  console.log(dayOfTheWeek);
+  document.getElementById('result').innerHTML = Math.round(dayOfTheWeek);
