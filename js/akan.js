2994ad1ae797eb498a3ca120329c35424c5b0f96
Imanirakarama Alexianne <imanirax@yahoo.fr>
2019-07-19 11:16:13 +0200

    created variables for day,month,year and century of birth

---
+++ js/akan.js
@@ -0,0 +1,6 @@
+var dayOfBirth=parseFloat(document.getElementById("day").value);
+var monthOfBirth=parseFloat(document.getElementById("month").value);
+var yearOfBirth=parseFloat(document.getElementById("year").value);
+var cc=(year-1)/100+1;
+var dayOfTheWeek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
+  
