7b81bd3e28c9df72fcf28e80f4a2575ed5429cab
Kobusingye Naume <kobnaome@gmail.com>
2019-07-21 23:46:57 +0200

    added a README.md and made changes to both index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,41 @@
 function akanNames(){
-    var dayOfBirth=parseFloat(document.getElementById("days").innerHTML);
-    var monthOfBirth=parseFloat(document.getElementById("months").innerHTML);
-    var yearOfBirth=parseFloat(document.getElementById("years").innerHTML);
-    var cc=(yearOfBirth-1)/100+1;
-    var dayOfTheWeek =  ( ( (CC/4) -2*CC-1) + ((5*yearOfBirth/4) ) + ((26*(monthOfBirth+1)/10)) + dayOfBirth ) % 7;
-    console.log(dayOfTheWeek);
-    document.getElementById('output').innerHTML = Math.round(dayOfTheWeek);
+    var date=parseFloat(document.getElementById("days").value);
+    var month=parseFloat(document.getElementById("months").value);
+    var year=parseFloat(document.getElementById("years").value);
+    var CC=(year-1)/100+1;
+    var day = ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date )%7;
 
-    var male = ["kwasi", "kwadwo", "kwabena", "kwaku", "yaw", "kofi", "kwame"];
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var collect = document.getElementById('gender').value;
+    document.getElementById('output').innerHTML = Math.round(day);
 
-   if (collect === "male") {
-        console.log(male[Math.round(dayOfTheWeek)]);
-        document.getElementById('name').innerHTML = male[Math.round(dayOfTheWeek)]; 
-    }
-    else if (collect === "female") {
-    console.log(female[Math.round(dayOfTheWeek)]);
-    document.getElementById('name').innerHTML = female[Math.round(dayOfTheWeek)];
-    } 
-    else {
-            alert("invalid outuput");
+    // var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // var theDay = day[date.getDay()];
+    // console.log(theDay);
+
+    var male = ["kwasi","kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
+    var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var gender = document.getElementById('gender').value;
+
+    if (year == "" && gender=="")
+        {
+        alert("Please fill the form");
         }
-        
+    else if (month == "" && day=="")
+        {
+        alert("Please fill the boxes");
+        }
+    else if (date<=0 || date>31) {
+        alert("enter a valid day");  
+      }
+    else if (month<= 0 || month> 12)  {
+        alert("enter a valid month");  
+      }
+    else if (gender === "female") {
+        document.getElementById('name').innerHTML = female[Math.round(day)]; 
+      }
+    else {
+        document.getElementById('name').innerHTML = male[Math.round(day)];
+        }
+        // else{
+        //     alert(done);
+        // }
 }
